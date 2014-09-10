e80b03bd0b63f3b8ac94ecc9dd160e23e8fa30be
Caio Nardelli <caio.nardelli@hotmail.com>
2014-09-10 15:14:43 -0300

    Updating class Contato

--- src/model/Contato.java
+++ src/model/Contato.java
@@ -2,15 +2,14 @@ package model;
 
 public class Contato {
 	
-	
 	private String nome;
 	private String telefone;
 	private String endereco;
 	private String email;
-	
+	private int idade;
 	
 	public Contato(){
-		
+
 	}
 
 }
