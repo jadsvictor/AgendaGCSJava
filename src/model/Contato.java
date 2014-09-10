014cfdb34ac9951e7ce7f1f3c16764a9d0ed5b1c
ViniciusRangel <vinirmv23@gmail.com>
2014-09-10 15:25:22 -0300

    adicionando getter and setters

--- src/model/Contato.java
+++ src/model/Contato.java
@@ -11,5 +11,47 @@ public class Contato {
 	public Contato(){
 
 	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	
 
 }
