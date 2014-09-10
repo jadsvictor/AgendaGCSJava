fba0408caa4dc7a9f10bde129b2828d4c205a695
CaioIcy <caio.nardelli@hotmail.com>
2014-09-10 14:55:27 -0300

    Creating class Contato

---
+++ src/model/Contato.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Contato {
+	
+	
+	private String nome;
+	private String telefone;
+	private String endereco;
+	private String email;
+	
+	
+	public Contato(){
+		
+	}
+
+}
